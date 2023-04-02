7408490746a1db2f0d11ae637ebfb7c37c266b4c
GoshaSerbin <gosha_serbin00@mail.ru>
2023-04-02 16:52:32 +0300

    DEL: testing: temporarily removed tests

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -1,37 +1,38 @@
 // Copyright 2023 Gosha Serbin
+
 #include <gtest/gtest.h>
 #include "Application.hpp"
 
-TEST(getFieldTest, getExistingFields) {
-    std::string line("f0\tf1\tf2\t\n");
-    EXPECT_EQ(Application::getField(0, line), "f0");
-    EXPECT_EQ(Application::getField(1, line), "f1");
-    EXPECT_EQ(Application::getField(2, line), "f2");
-}
+// TEST(getFieldTest, getExistingFields) {
+//     std::string line("f0\tf1\tf2\t\n");
+//     EXPECT_EQ(Application::getField(0, line), "f0");
+//     EXPECT_EQ(Application::getField(1, line), "f1");
+//     EXPECT_EQ(Application::getField(2, line), "f2");
+// }
 
-TEST(getFieldTest, getNonExistingFields) {
-    EXPECT_EQ(Application::getField(2, "0\t1"), "");
-    EXPECT_EQ(Application::getField(1, "hello"), "");
-    EXPECT_EQ(Application::getField(5, "h\te\tl\tl\to"), "");
-}
+// TEST(getFieldTest, getNonExistingFields) {
+//     EXPECT_EQ(Application::getField(2, "0\t1"), "");
+//     EXPECT_EQ(Application::getField(1, "hello"), "");
+//     EXPECT_EQ(Application::getField(5, "h\te\tl\tl\to"), "");
+// }
 
-TEST(fillMapGenderByIDTest, basicInput) {
-    std::unordered_map<size_t, std::string> genderByID;
-    std::stringstream ss;
-    ss << "0\tMale\n1\tFemale\n2\tOther";
-    Application::fillMapGenderByID(ss, &genderByID);
-    EXPECT_EQ(genderByID.size(), 3);
-    EXPECT_EQ(genderByID.at(0), "Male");
-    EXPECT_EQ(genderByID.at(1), "Female");
-    EXPECT_EQ(genderByID.at(2), "Other");
-}
+// TEST(fillMapGenderByIDTest, basicInput) {
+//     std::unordered_map<size_t, std::string> genderByID;
+//     std::stringstream ss;
+//     ss << "0\tMale\n1\tFemale\n2\tOther";
+//     Application::fillMapGenderByID(ss, &genderByID);
+//     EXPECT_EQ(genderByID.size(), 3);
+//     EXPECT_EQ(genderByID.at(0), "Male");
+//     EXPECT_EQ(genderByID.at(1), "Female");
+//     EXPECT_EQ(genderByID.at(2), "Other");
+// }
 
-TEST(printArtistTest, emptyInput) {
-    std::unordered_map<size_t, std::string> genderByID;
-    std::string input("0\t1\t2\t3\t\\N\t\\N\t\\N\t7\t8\t9\t10\t11\t\\N");
-    std::stringstream ssOut;
-    Application::printArtist(input, genderByID, ssOut);
-    std::string out;
-    ssOut >> out;
-    EXPECT_EQ(out, "");
-}
+// TEST(printArtistTest, emptyInput) {
+//     std::unordered_map<size_t, std::string> genderByID;
+//     std::string input("0\t1\t2\t3\t\\N\t\\N\t\\N\t7\t8\t9\t10\t11\t\\N");
+//     std::stringstream ssOut;
+//     Application::printArtist(input, genderByID, ssOut);
+//     std::string out;
+//     ssOut >> out;
+//     EXPECT_EQ(out, "");
+// }
